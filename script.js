4ab7baf5c05f3f0f8c60705503c89d911b040af9
Jc6300 <joshcotton93@gmail.com>
2024-01-15 19:49:20 -0600

    updated

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+class Player {
+	constructor() {
+		this.position = {
+			x: 10,
+			y: 400,
+		};
+		this.velocity = {
+			x: 0,
+			y: 0,
+		};
+		this.width = 40;
+		this.height = 40;
+	}
+}
